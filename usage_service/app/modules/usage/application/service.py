4cc461d4f78d3afce44161b057b161eb4b256e73
MTGMODS <bogdan.mtg@gmail.com>
2026-06-05 22:03:19 +0300

    Refactor usage_service: remove old modules structure

--- usage_service/app/modules/usage/application/service.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from ..infrastructure.repository import LaunchRepository
-
-class UsageService:
-    def __init__(self, db: Session):
-        self.repo = LaunchRepository(db)
-
-    def log_launch(self, version: str, device: str, server_id: str, country: str):
-        return self.repo.save(version, device, server_id, country)
