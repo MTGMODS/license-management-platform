3845c126c9ae38989893aa00b03fade53291ef7b
MTGMODS <bogdan.mtg@gmail.com>
2026-06-06 11:56:15 +0300

    Update user registration endpoint to return TokenResponse with access and refresh tokens

--- user_service/app/api/routes.py
+++ user_service/app/api/routes.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.shared.database import get_db
 from app.application.service import UserService
-from app.domain.models import User, UserLinkDTO
+from app.domain.models import UserLinkDTO, TokenResponse
+from app.application.jwt_utils import create_access_token, create_refresh_token
 
 router = APIRouter(prefix="/api/v1/users", tags=["Users Service"])
 
-@router.post("", response_model=User)
+@router.post("", response_model=TokenResponse)
 async def register_or_update_user(payload: UserLinkDTO, db: AsyncSession = Depends(get_db)):
     service = UserService(db)
     
@@ -17,4 +18,11 @@ async def register_or_update_user(payload: UserLinkDTO, db: AsyncSession = Depen
         avatar_url=payload.avatar_url
     )
     
-    return user
+    access_tk = create_access_token(user_id=user.id, role=user.role)
+    refresh_tk = create_refresh_token(user_id=user.id)
+    
+    return TokenResponse(
+        access_token=access_tk,
+        refresh_token=refresh_tk,
+        user=user
+    )
