a29e789bc37c09ad96ad6623cde1c281d6e0b235
MTGMODS <bogdan.mtg@gmail.com>
2026-06-21 19:12:28 +0300

    Update link_social_account to use LinkSocialPayload instead of SocialIDPayload

--- user_service/app/api/user_routes.py
+++ user_service/app/api/user_routes.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.shared.database import get_db
 from app.application.user_service import UserService
-from app.domain.models import SocialIDPayload, SyncPayload, User
+from app.domain.models import LinkSocialPayload, User
 from app.application.jwt_utils import get_current_user_id
 
 router = APIRouter(prefix="/api/v1/users", tags=["Users Service"])
@@ -19,7 +19,7 @@ async def delete_my_account(current_user_id: int = Depends(get_current_user_id),
     return await service.delete_my_account(current_user_id)
 
 @router.post("/me/link", response_model=User)
-async def link_social_account(payload: SocialIDPayload, current_user_id: int = Depends(get_current_user_id), db: AsyncSession = Depends(get_db)):
+async def link_social_account(payload: LinkSocialPayload, current_user_id: int = Depends(get_current_user_id), db: AsyncSession = Depends(get_db)):
     service = UserService(db)
     return await service.link_social(
         user_id=current_user_id,
