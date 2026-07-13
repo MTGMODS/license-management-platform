e07d228601e892631ea7dfbf2a7a055d8cd16baa
MTGMODS <bogdan.mtg@gmail.com>
2026-07-13 14:18:14 +0300

    Refactor message processing to use DistributionService for VIP file generation

--- distribution_service/app/infrastructure/messaging.py
+++ distribution_service/app/infrastructure/messaging.py
@@ -1,8 +1,7 @@
 import json
 import aio_pika
-from app.shared.database import AsyncSessionLocal
 from app.domain.schemas import GenerationPayloadDTO
-from app.application.service import FileGeneratorService
+from app.application.service import DistributionService
 
 async def process_message(message: aio_pika.IncomingMessage):
     async with message.process():
@@ -12,7 +11,6 @@ async def process_message(message: aio_pika.IncomingMessage):
             payload = GenerationPayloadDTO(**event_dict['payload'])
             print(f"[RabbitMQ] Task received for User {payload.user_id}")
             
-            async with AsyncSessionLocal() as db:
-                service = FileGeneratorService(db)
-                download_url = await service.build_lua_file(payload)
-                print(f"[RabbitMQ] Task completed. URL: {download_url}")
+            service = DistributionService()
+            download_url = await service.build_vip_file(payload)
+            print(f"[RabbitMQ] Task completed. URL: {download_url}")
