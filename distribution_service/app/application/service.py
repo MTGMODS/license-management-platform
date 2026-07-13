fed75263e3208f2106c22028feb7a0998af878b8
MTGMODS <bogdan.mtg@gmail.com>
2026-07-13 14:16:03 +0300

    Refactor DistributionService to build VIP files with expiration logic and update template path

--- distribution_service/app/application/service.py
+++ distribution_service/app/application/service.py
@@ -1,31 +1,42 @@
-import os
-import aiofiles
-from datetime import datetime, timezone
-from sqlalchemy.ext.asyncio import AsyncSession
-
+import os, re, uuid, aiofiles
 from app.domain.schemas import GenerationPayloadDTO
-from app.infrastructure.repository import FileGeneratorRepository
 
-BUILDS_DIR = "builds"
-BASE_TEMPLATE_PATH = "app/assets/base_helper.lua" 
+BUILDS_DIR = "app/builds"
+BASE_TEMPLATE_PATH = "app/files/vip/Arizona Helper.lua" 
 
 os.makedirs(BUILDS_DIR, exist_ok=True)
 
-class FileGeneratorService:
-    def __init__(self, db: AsyncSession):
-        self.repo = FileGeneratorRepository(db)
-
-    async def build_lua_file(self, payload: GenerationPayloadDTO) -> str:
+class DistributionService:
+    async def build_vip_file(self, payload: GenerationPayloadDTO) -> str:
         if not os.path.exists(BASE_TEMPLATE_PATH):
             raise RuntimeError(f"Base LUA template is missing at {BASE_TEMPLATE_PATH}")
 
-        try:
-            db_log = await self.repo.log_generation(key=payload.key, user_id=payload.user_id, filename=BASE_TEMPLATE_PATH)
-            print(f"[Build] Success for User {payload.user_id}. DB Log ID: {db_log.id}")
-        except Exception as e:
-            print(f"[Build] Failed to save log in DB: {e}")
+        async with aiofiles.open(BASE_TEMPLATE_PATH, mode='r', encoding='cp1251') as f:
+            content = await f.read()
+
+        if payload.expire_date:
+            expire = payload.expire_date
+            time_bomb = f"if os.time() > os.time{{year={expire.year}, month={expire.month}, day={expire.day}}} then os.remove(thisScript().path) end"
+
+            if " function main" in content:
+                content = re.sub(
+                    f"function main\(\)", 
+                    f"function main() {time_bomb}; ",
+                    content
+                )
+            elif "\nfunction main" in content:
+                content += f"\n\n{time_bomb}"
+
+        download_token = uuid.uuid4().hex[:12]
+        filepath = os.path.join(BUILDS_DIR, f"{download_token}.lua")
         
-        return f"http://localhost:8005/downloads/{BASE_TEMPLATE_PATH}"
+        async with aiofiles.open(filepath, mode="w", encoding="cp1251") as f:
+            await f.write(content)
+            
+        print(f"[Build] VIP file ready for User {payload.user_id}. Token: {download_token}")
+        
+        # return f"https://api.mtgmods.com/api/v1/files/downloads/vip/{download_token}"
+        return f"http://localhost:8005/downloads/vip/{download_token}"
 
     @staticmethod
     def remove_file(path: str):
