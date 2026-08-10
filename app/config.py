d579647c577c7f75072362c1c1f4a53e01e15f94
MTGMODS <bogdan.mtg@gmail.com>
2026-08-10 11:54:52 +0300

    Add config.py to manage environment variables for the bot

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+DISCORD_GUILD_ID = int(os.getenv("DISCORD_GUILD_ID"))
+VIP_ROLE_ID = int(os.getenv("VIP_ROLE_ID"))
+
+BACKEND_API_URL = os.getenv("BACKEND_API_URL")
+RABBITMQ_URL = os.getenv("RABBITMQ_URL")
