2b1fd54aba5b35145a196a82f7f6d1284b6cc13d
MTGMODS <bogdan.mtg@gmail.com>
2026-08-10 11:56:43 +0300

    Add VIP and chat channel IDs to config.py

--- app/config.py
+++ app/config.py
@@ -6,6 +6,8 @@ load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 DISCORD_GUILD_ID = int(os.getenv("DISCORD_GUILD_ID"))
 VIP_ROLE_ID = int(os.getenv("VIP_ROLE_ID"))
+VIP_CHANNEL_ID = int(os.getenv("VIP_CHANNEL_ID"))
+CHAT_CHANNEL_ID = int(os.getenv("CHAT_CHANNEL_ID"))
 
 BACKEND_API_URL = os.getenv("BACKEND_API_URL")
 RABBITMQ_URL = os.getenv("RABBITMQ_URL")
